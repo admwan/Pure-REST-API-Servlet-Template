ddc0ea45e3d291001eb898f8ea9e693c38b0911d
root <mail@fredwan.net>
2024-01-02 13:20:29 +0100

    First commit

---
+++ src/main/java/com/minhct/servlet/api/dao/BaseDao.java
@@ -0,0 +1,26 @@
+package com.minhct.servlet.api.dao;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class BaseDao {
+
+    private static final Logger LOGGER = LogManager.getLogger(BaseDao.class);
+
+    protected Connection connection;
+
+    protected BaseDao() {
+/*
+ *      NOT IMPLEMENTED YET   
+ *      try {
+ 
+            // Get connection from pool
+        } catch (SQLException e) {
+            LOGGER.error("Error creating SQL connection from pool", e);
+        }
+*/
+    }
+}
